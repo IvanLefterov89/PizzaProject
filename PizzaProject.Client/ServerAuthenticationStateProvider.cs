f23bd3d34b3b2c614735eab96cd35202a1f53276
IvanLefterov89 <ivanlefterov1@gmail.com>
2020-04-16 21:46:30 +0300

    signing in with Twitter

--- PizzaProject.Client/ServerAuthenticationStateProvider.cs
+++ PizzaProject.Client/ServerAuthenticationStateProvider.cs
@@ -1,4 +1,5 @@
-﻿using System.Security.Claims;
+﻿using System.Net.Http;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.Authorization;
 
@@ -6,6 +7,12 @@ namespace BlazingPizza.Client
 {
     public class ServerAuthenticationStateProvider : AuthenticationStateProvider
     {
+        private readonly HttpClient _httpClient;
+
+        public ServerAuthenticationStateProvider(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
         public override async Task<AuthenticationState> GetAuthenticationStateAsync()
         {
             var claim = new Claim(ClaimTypes.Name, "Fake user");
